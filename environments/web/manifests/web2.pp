49b515cb98128faa9a48c15e5b876abb46266215
mirandarfsm <harripobre@gmail.com>
2016-09-15 07:22:20 -0300

    add 2 cookbook

---
+++ environments/web/manifests/web2.pp
@@ -0,0 +1,53 @@
+# Java
+file { "/opt/jdk":
+	owner => vagrant,
+	mode => 0755, 
+	#recurse => true,
+	#ensure => 'directory',
+	source => "file:///jdk",
+}
+
+exec { "set-java-home":
+	command => "update-alternatives --install /usr/bin/java java /opt/jdk/bin/java 1000",
+	path => "/usr/bin/",
+}
+
+exec { "set-java-home":
+	command => "update-alternatives --install /usr/bin/javac javac /opt/jdk/bin/javac 1000",
+	path => "/usr/bin/",
+}
+
+# Maven
+file { "/opt/maven":
+	owner => vagrant,
+	mode => 0755, 
+	source => "file:///maven",
+}
+
+exec { "set-maven-home":
+	command => "update-alternatives --install /usr/bin/mvn mvn /opt/maven/bin/mvn 1000",
+	path => "/usr/bin/",
+}
+
+# Tomcat
+file { "/opt/tomcat":
+	owner => vagrant,
+	mode => 0755, 
+	source => "file:///tomcat",
+}
+
+# eclipse
+file { "/opt/eclipse":
+	owner => vagrant,
+	mode => 0755, 
+	source => "file:///eclipse",
+}
+
+file { "/home/vagrant/.local/share/applications/eclipse.desktop":
+	owner => vagrant,
+	group => vagrant,
+	mode => 0755, 
+	source => "file:///eclipse.desktop",
+}
+
+# Graphical User Interface
