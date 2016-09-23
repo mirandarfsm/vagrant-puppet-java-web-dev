b6292f3f4f652a234c94346b00e1d833361a4060
mirandarfsm <harripobre@gmail.com>
2016-09-23 10:22:38 -0300

    added modules

--- environments/web/manifests/web_file.pp
+++ environments/web/manifests/web_file.pp
@@ -1,31 +1,73 @@
-# Java
-file { "/opt/jdk":
+# Alter locale in Debian and Ubuntu
+file { "/etc/locale.gen":
 	owner => vagrant,
 	mode => 0755, 
-	#recurse => true,
-	#ensure => 'directory',
-	source => "file:///jdk",
+	source => "puppet:///modules/locale/locale.gen",
+}
+
+exec { "locale-generation":
+	command => "locale-gen",
+	path => ["/usr/sbin/","/usr/bin/","/bin/"],
+	require => File["/etc/locale.gen"]
+}
+
+file { "/etc/default/locale":
+	owner => vagrant,
+	mode => 0755, 
+	source => "puppet:///modules/locale/locale",
+}
+
+file { "/etc/apt/source.list":
+	owner => vagrant,
+	mode => 0755, 
+	source => "puppet:///modules/apt/source.list",
+	require => File["/etc/default/locale"]
+}
+
+# Update apt repository
+exec { "apt-update":
+	user => root,
+	command => "apt-get update",
+	path => "/usr/bin/",
+	require => File["/etc/apt/source.list"],
+}
+
+# Graphical User Interface
+# Installing xfce4 like a User Interface.
+# You can choose other like: xfce4-goodies, LXDE, IceWM, Openbox and etc...
+package { "xfce4-goodies":
+	ensure => "latest",
+}
+
+# Java
+file { "/opt/jdk":
+	mode => 0755, 
+	recurse => true,
+	source => "puppet://modules/java/jdk1.8u36/",
 }
 
 exec { "set-java-home":
 	command => "update-alternatives --install /usr/bin/java java /opt/jdk/bin/java 1000",
+	unless => "java -version",
 	path => "/usr/bin/",
 }
 
 exec { "set-javac-home":
 	command => "update-alternatives --install /usr/bin/javac javac /opt/jdk/bin/javac 1000",
+	unless => "javac -version",
 	path => "/usr/bin/",
 }
 
 # Maven
 file { "/opt/maven":
-	owner => vagrant,
 	mode => 0755, 
-	source => "file:///maven",
+	recurse => true,
+	source => "puppet://modules/maven/apache-maven-3.2/",
 }
 
 exec { "set-maven-home":
 	command => "update-alternatives --install /usr/bin/mvn mvn /opt/maven/bin/mvn 1000",
+	unless => "mvn -version",
 	path => "/usr/bin/",
 }
 
@@ -33,26 +75,19 @@ exec { "set-maven-home":
 file { "/opt/tomcat":
 	owner => vagrant,
 	mode => 0755, 
-	source => "file:///tomcat",
+	source => "puppet://modules/tomcat/apache-tomcat-8/",
 }
 
 # Eclipse
 file { "/opt/eclipse":
 	owner => vagrant,
 	mode => 0755, 
-	source => "file:///eclipse",
+	source => "puppet://modules/eclipse/eclipse-neon/",
 }
 
 file { "/home/vagrant/.local/share/applications/eclipse.desktop":
 	owner => vagrant,
 	group => vagrant,
 	mode => 0755, 
-	source => "file:///eclipse.desktop",
-}
-
-# Graphical User Interface
-# Installing xfce4 like a User Interface.
-# You can choose other like: xfce4, LXDE, IceWM, Openbox and etc...
-package { "xfce4":
-	ensure => "latest",
+	source => "puppet://modules/eclipse/eclipse.desktop",
 }
--- environments/web/manifests/web_package.pp
+++ environments/web/manifests/web_package.pp
@@ -1,17 +1,46 @@
+# Alter locale in Debian and Ubuntu
+file { "/etc/locale.gen":
+	owner => vagrant,
+	mode => 0755, 
+	source => "puppet:///modules/locale/locale.gen",
+}
+
+exec { "locale-generation":
+	command => "locale-gen",
+	path => ["/usr/sbin/","/usr/bin/","/bin/"],
+	require => File["/etc/locale.gen"]
+}
+
+file { "/etc/default/locale":
+	owner => vagrant,
+	mode => 0755, 
+	source => "puppet:///modules/locale/locale",
+}
+
+file { "/etc/apt/source.list":
+	owner => vagrant,
+	mode => 0755, 
+	source => "puppet:///modules/apt/source.list",
+	require => File["/etc/default/locale"]
+}
+
 # Update apt repository
 exec { "apt-update":
 	user => root,
 	command => "apt-get update",
 	path => "/usr/bin/",
-} ->
+	require => File["/etc/apt/source.list"],
+}
 
 # Installing xfce4 like a User Interface.
-# You can choose other like: xfce4, LXDE, IceWM, Openbox and etc...
+# You can choose other like: xfce4-goodies, LXDE, IceWM, Openbox and etc...
 package { "xubuntu-desktop":
 	ensure => "latest",
+	require => Exec["apt-update"],
 }
 
 # Installing environment for java development
-package {"firefox","openjdk-7-jdk","maven","tomcat7","eclipse"]
+package {["firefox","openjdk-7-jdk","maven","tomcat7","eclipse"]:
 	ensure => "latest",
+	require => Exec["apt-update"],
 }
